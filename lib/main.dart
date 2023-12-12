7ede6ba1da0ba74688c20eeb92b55b970ba0c0d2
Mark Deyarmond <deyarmondmark@gmail.com>
2023-12-12 14:04:27 -0400

    Removed entry splash screen and created bottom nav to page switching

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,62 @@
 import 'package:flutter/material.dart';
 import '../UI/homepage.dart';
-import '../UI/splashscreen.dart';
+import '../UI/teamstats.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This section will define all the pages used by the app, shown on bottom nav
+  final List<Widget> appPages = [
+    const HomePage(),
+    const TeamStats(),
+  ];
+
+  /*Sets the initial index on bottom nav to 0; 'HomePage'
+  *Note to self: prefixing the variable name with an underscore denotes that the
+  *variable is private to this class
+  */
+  int _selectedNavIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedNavIndex = index;
+    });
+  }
+
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, // Hides the "debug" corner label
-      initialRoute: '/',
-      theme: new ThemeData(scaffoldBackgroundColor: const Color(606060)),
-      routes: {
-        '/': (context) => const SplashScreen(),
-        '/home': (context) => const HomePage(),
-      },
+      theme: ThemeData(scaffoldBackgroundColor: const Color(0xFF606060)),
+      home: Scaffold(
+        body: appPages[_selectedNavIndex], // Will show the selected nav page
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.sports_hockey),
+              label: 'Games',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.auto_graph),
+              label: 'Team Stats',
+            )
+          ],
+          currentIndex: _selectedNavIndex,
+          selectedItemColor: Color(0xFF202020),
+          onTap: _onItemTapped,
+        ),
+      ),
     );
   }
 }
