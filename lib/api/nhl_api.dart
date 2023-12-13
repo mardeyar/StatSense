c98d709e50032f73755bd0cc3833caf7fe3f3a6e
Mark Deyarmond <deyarmondmark@gmail.com>
2023-12-13 14:27:26 -0400

    Built API call for standings

--- lib/api/nhl_api.dart
+++ lib/api/nhl_api.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class NHLApi {
+  // Builds the method to fetch data on current & upcoming games
   static Future<Map<String, dynamic>> fetchScheduleData(String formattedDate) async {
     final response = await http.get(
       Uri.parse('https://api-web.nhle.com/v1/schedule/$formattedDate'),
@@ -11,6 +12,19 @@ class NHLApi {
       return json.decode(response.body);
     } else {
       throw Exception('Failed to load NHL Game data.');
+    }
+  }
+
+  // Builds the methods to fetch data on current league standings
+  static Future<Map<String, dynamic>> fetchTeamStandings() async {
+    final standingsResponse = await http.get(
+      Uri.parse('https://api-web.nhle.com/v1/standings/now'),
+    );
+
+    if (standingsResponse.statusCode == 200) {
+      return json.decode(standingsResponse.body);
+    } else {
+      throw Exception('Failed to load NHL standings');
     }
   }
 }
