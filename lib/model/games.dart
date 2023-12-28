66565b7f55d6703b612f162938b38266fa444974
Mark Deyarmond <deyarmondmark@gmail.com>
2023-12-28 16:06:23 -0400

    Added json methods for data parsing

--- lib/model/games.dart
+++ lib/model/games.dart
@@ -1,15 +1,3 @@
-class Game {
-  final String date;
-  final String awayTeam;
-  final String homeTeam;
-
-  Game({
-    required this.date,
-    required this.awayTeam,
-    required this.homeTeam,
-  });
-}
-
 class GameDate {
   String date;
   int numberOfGames;
@@ -22,4 +10,52 @@ class GameDate {
     this.offDay = true,
     required this.gameList,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'date': date,
+      'numberOfGames': numberOfGames,
+      'offDay': offDay,
+      'gameList': gameList.map((game) => game.toJson()).toList(),
+    };
+  }
+
+  factory GameDate.fromJson(Map<String, dynamic> json) {
+    return GameDate(
+      date: json['date'],
+      numberOfGames: json['numberOfGames'],
+      offDay: json['offDay'],
+      gameList: (json['gameList'] as List<dynamic>)
+          .map((gameJson) => Game.fromJson(gameJson))
+          .toList(),
+    );
+  }
+}
+
+class Game {
+  final String date;
+  final String awayTeam;
+  final String homeTeam;
+
+  Game({
+    required this.date,
+    required this.awayTeam,
+    required this.homeTeam,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'date': date,
+      'awayTeam': awayTeam,
+      'homeTeam': homeTeam,
+    };
+  }
+
+  factory Game.fromJson(Map<String, dynamic> json) {
+    return Game(
+      date: json['date'],
+      awayTeam: json['awayTeam'],
+      homeTeam: json['homeTeam'],
+    );
+  }
 }
