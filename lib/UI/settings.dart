db1928badc3f4e15f3a66852b135a6c0f73e6b40
Mark Deyarmond <deyarmondmark@gmail.com>
2024-01-04 10:52:00 -0400

    Added about and bug report/feature request pages

--- lib/UI/settings.dart
+++ lib/UI/settings.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:nhl/utils/styles.dart';
+import '../UI/about.dart';
+import '../UI/contact.dart';
 
 class Settings extends StatelessWidget {
+  final List<Widget> appPages = [
+    About(),
+    Contact(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,10 +20,46 @@ class Settings extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.all(10.0),
         child: SafeArea(
-          child: Text('Need to add some settings here',
-          style: BodyTextStyle.bodyTextStyleReg
+          child: ListView(
+            children: <Widget>[
+              _buildCard(
+                context,
+                'About',
+                  () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => About()),
+                  );
+                },
+              ),
+              _buildCard(
+                context,
+                'Bug Reports/Feature Requests',
+                    () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Contact()),
+                  );
+                },
+              ),
+            ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildCard(BuildContext context, String title, VoidCallback onTap) {
+    return Card(
+      child: ListTile(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(title),
+            Icon(Icons.arrow_forward_ios),
+          ],
+        ),
+        onTap: onTap,
       ),
     );
   }
