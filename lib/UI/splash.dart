0776305542b974ac70ad56e2fdc37643fccdef69
Mark Deyarmond <deyarmondmark@gmail.com>
2024-01-06 14:28:07 -0400

    Changed app icon and added splash screen

---
+++ lib/UI/splash.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/splash.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
